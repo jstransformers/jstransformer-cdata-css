2d1c4ca196bae70542e3e1a5a583c682ca582bd1
Rob Loach <robloach@gmail.com>
2016-12-09 13:27:45 -0500

    Add xo as coding style (#37)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-'use strict';
+'use strict'
 
+<<<<<<< HEAD
 var re = /\]\]>/g;
 
 exports.name = 'cdata-css';
@@ -8,4 +9,12 @@ exports.outputFormat = 'html';
 exports.render = function (str, options) {
   var escaped = str.replace(re, "]]]]><![CDATA[>");
   return '/*<![CDATA[*/\n' + escaped + '\n/*]]>*/';
+=======
+exports.name = 'foo'
+exports.inputFormats = ['foo', 'foobar']
+exports.outputFormat = 'html'
+
+exports.render = function (str) {
+  return str
+>>>>>>> Add xo as coding style (#37)
 }
